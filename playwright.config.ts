be8d24415b9b757259237ec50b73931089e2ba72
Ekemini Udoh <ekemini.udoh1790@gmail.com>
2024-05-02 13:51:36 +0100

    setup: setting up the customised fixtures, write and run test cases successfully

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+
 export default defineConfig({
-  testDir: './tests',
+  testDir: 'src/Test/',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
---
+++ src/Test/UI-test.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "../fixtures/fixtures";
+import {LoginPage} from "../pages/login.page";
+
+test.describe("Items sorting test", () => {
+
+    test.beforeEach("Go to https://www.saucedemo.com/ and login " , async ({ loginPage }) => {
+        await loginPage.launchURL();
+        await loginPage.login("standard_user", "secret_sauce");
+    });
+
+    test("2. Verify that the items are sorted by Name ( A -> Z )", async ({ itemsPage }) => {
+        await itemsPage.verifyItemsSortedByNameAsc();
+    })
+    test("3. Change the sorting to Name ( Z -> A)", async ({ itemsPage }) => {
+        await itemsPage.sortItemsByNameDesc();
+    })
+    test("4. Verify that the items are sorted correctly", async ({ itemsPage }) => {
+        await itemsPage.verifyItemsSortedByNameDesc();
+    });
+});
